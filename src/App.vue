24eb7864014f5cdede1fa32aec57c97e0cd7cc30
Paul McManus <paulmcmanus2021@hotmail.com>
2019-12-18 15:33:23 +0000

    MVP

--- src/App.vue
+++ src/App.vue
@@ -3,29 +3,37 @@
     <h1>Countries</h1>
     <div class="main-container">
       <countries-list v-bind:countries='countries'></countries-list>
-
+      <country-detail v-bind:country='selectedCountry'></country-detail>
     </div>
   </div>
 </template>
 
 <script>
 import CountriesList from './components/CountriesList.vue';
+import { eventBus } from './main.js'
+import CountryDetail from './components/countryDetail.vue';
 
 export default {
   name: 'app',
   data(){
     return {
       countries: [],
+      selectedCountry: null
     }
   },
   mounted(){
     fetch('https://restcountries.eu/rest/v2/all')
     .then(res => res.json())
     .then(countries => this.countries = countries)
+
+    eventBus.$on('country-selected', (country) =>{
+      this.selectedCountry = country
+    })
   },
 
   components: {
-    "countries-list": CountriesList
+    "countries-list": CountriesList,
+    "country-detail": CountryDetail,
   }
 }
 
@@ -33,5 +41,8 @@ export default {
 </script>
 
 <style>
-
+  .main-container {
+    display: flex;
+    justify-content: space-between;
+  }
 </style>
